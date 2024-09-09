8ffbb958bf1e8118952a60f822ecc2cf516a4414
fogopikrishna <forgopikrishna@gmail.com>
2024-09-09 13:21:19 +0530

    Update streamlit_app.py
    Modied version of custom smoothie order form.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -8,7 +8,9 @@ st.write(
 )
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The Name on Smoothie Will be", name_on_order)
-session = get_active_session()
+#session = get_active_session()
+cnx=st.connection("snowflake")
+sessions=cnx.session()
 import streamlit as st
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'));
 #st.dataframe(data=my_dataframe, use_container_width=True);
